740e4d2cda3a0a1cb01afcf4e43b562f097bab6a
Ali Maihoob <alimayhop6@gmail.com>
2023-06-25 21:24:18 +0400

    Add fixed extra payment

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -39,12 +39,16 @@ class LoanController extends Controller
         $loanTerm = $request->input('loan_term');
         $monthlyPayment = LoanHelper::calculateMonthlyPayment($loanAmount, $interestRate, $loanTerm);
 
+        // Calculate the fixed extra payment amount (if any)
+        $fixedExtraPayment = $request->input('fixed_extra_payment');
+
         // Store the loan data in the database
         $loan = new Loan([
             'loan_amount' => $loanAmount,
             'interest_rate' => $interestRate,
             'loan_term' => $loanTerm * 12,
             'monthly_payment' => $monthlyPayment,
+            'fixed_extra_payment' => $fixedExtraPayment,
             'user_id' => Auth::id(),
         ]);
         if($loan->save()) {
